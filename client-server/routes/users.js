5f54a7e100afc34bfa4152b66d01de643448f036
Ana Beatriz <anabiawalker@gmail.com>
2022-03-09 17:33:44 -0300

    Adicionando acesso a api rest

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -1,11 +1,22 @@
 var express = require('express');
+var restify = require('restify-clients');
+var assert = require('assert');
 var router = express.Router();
+
+//Creates a JSON client
+var client = restify.createJsonClient({
+  url: 'http://localhost:4000' //endereço do servidor. Declara onde está o serviço
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-    res.send('respond with a resource');
-    
+  //Monta o link completo com /users. Mostra a rota
+  client.get('/users', function(err, request, response, obj){
+    assert.ifError(err)
+    //Joga na tela
+    res.end(JSON.stringify(obj, null, 2))
+  })
 });
 
 module.exports = router;
