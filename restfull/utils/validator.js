da324902cb73ab1ba41be0ccc4f8402feae4230c
Ana Beatriz <anabiawalker@gmail.com>
2022-03-09 17:54:04 -0300

    Adequando os dados salvos para o servidor

--- restfull/utils/validator.js
+++ restfull/utils/validator.js
@@ -1,8 +1,8 @@
 module.exports = {
     user: (app, req, res)=>{
         //Verifica os campos do body (nome do campo, mensagem de erro)
-        req.assert('name', 'O nome é obrigatório.').notEmpty();
-        req.assert('email', 'O e-mail está inválido.').notEmpty().isEmail();
+        req.assert('_name', 'O nome é obrigatório.').notEmpty();
+        req.assert('_email', 'O e-mail está inválido.').notEmpty().isEmail();
 
         let errors = req.validationErrors();
         if(errors){
